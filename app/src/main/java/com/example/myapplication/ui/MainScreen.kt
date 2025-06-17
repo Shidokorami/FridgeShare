99baf4e5844771167cfd92de6b8b24d979c1f075
Shidokorami <ZawiszaSolec@gmail.com>
2025-06-17 17:54:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/MainScreen.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen(){
+
+    Scaffold(
+
+    )
+}
+
+@Preview
+@Composable
+fun MainPreview(){
+    MainScreen()
+}
